5d9add30c226b5d761cbfaec1010441706c69418
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2023-02-15 17:16:35 -0300

    updateModule01

---
+++ ex03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $nome = "Emanuelle";
+        $sobrenome = "Viana"; //variavel
+        const PAIS = "Brasil"; //constante 
+
+        echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+    ?>
+    
+</body>
+</html>
