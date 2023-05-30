c1de8d45d816ca1dfe7374ac1bef67a2635b0046
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2023-05-30 15:34:39 -0300

    study: Criando formulários com HTML e PHP.

---
+++ cursoEmVídeo/ex05/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+
+<body>
+    <h1></h1>
+    <?php
+    $nome = "Emanuelle";
+    $sobrenome = "Viana";
+
+    //Aspas duplas
+    echo "$nome $sobrenome \u{1F596}";
+
+    //Aspas simples
+    echo '$nome $sobrenome \u{1F596}';
+
+    ?>
+</body>
+
+</html>
